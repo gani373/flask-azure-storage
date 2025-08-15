7842731518eae4cd1fc76ce62253fb0f4d9d55b1
gani373 <gg3466000@gmail.com>
2025-08-15 19:21:46 +0530

    Update models.py

--- FlaskExercise/models.py
+++ FlaskExercise/models.py
@@ -26,11 +26,7 @@ class Animal(db.Model):
             randomFilename = str(uuid.uuid1())
             filename = randomFilename + '.' + fileExtension
             try:
-                # TODO: Get a blob client and upload the blob
-                from azure.storage.blob import BlobServiceClient
-                blob_container = app.config['BLOB_CONTAINER']
-                storage_url = "https://{}.blob.core.windows.net/".format(app.config['BLOB_ACCOUNT'])
-                blob_service = BlobServiceClient(account_url=storage_url, credential=app.config['BLOB_STORAGE_KEY'])
+                # TODO: Get a blob client and upload the blo
                 blob_client = blob_service.get_blob_client(container=blob_container, blob=filename)
                 blob_client.upload_blob(file)
                
