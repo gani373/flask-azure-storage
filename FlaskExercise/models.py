9e9c7ec7cb59c1817071715e14e31c017df7bb7f
gani373 <gg3466000@gmail.com>
2025-08-15 19:20:13 +0530

    Update models.py

--- FlaskExercise/models.py
+++ FlaskExercise/models.py
@@ -27,10 +27,17 @@ class Animal(db.Model):
             filename = randomFilename + '.' + fileExtension
             try:
                 # TODO: Get a blob client and upload the blob
-                pass
+                from azure.storage.blob import BlobServiceClient
+                blob_container = app.config['BLOB_CONTAINER']
+                storage_url = "https://{}.blob.core.windows.net/".format(app.config['BLOB_ACCOUNT'])
+                blob_service = BlobServiceClient(account_url=storage_url, credential=app.config['BLOB_STORAGE_KEY'])
+                blob_client = blob_service.get_blob_client(container=blob_container, blob=filename)
+                blob_client.upload_blob(file)
+               
                 if self.image_path:
                     # TODO: Get a blob client and delete the previous blob
-                    pass
+                    blob_client = blob_service.get_blob_client(container=blob_container, blob=filename)
+                    blob_client.delete_blob()
             except Exception as err:
                 flash(err)
             self.image_path = filename
