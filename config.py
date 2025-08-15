1930a80fc8f61abb4e76f0afd5ee8be1c6fafa66
gani373 <gg3466000@gmail.com>
2025-08-15 22:38:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ class Config(object):
 
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'udacity.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'hello'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacityuser'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Udacity@1234'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or ''
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or ''
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'hellowo123'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'bwC9rXbhvcbHUbHOD4RrWXChBDxEQ0qBkGguUjjz44R+SF73o69Kamhk+qtqOQKTfxCVW/xrSpBn+AStvZLVdg=='
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or ''
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
